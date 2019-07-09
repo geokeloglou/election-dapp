4bd1d2401619578a001f4b289104e0cbcc9e4d6c
George Keloglou <geokeloglou@gmail.com>
2019-07-09 14:06:43 +0300

    list candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,16 +1,30 @@
 pragma solidity ^0.5.0;
 
 contract Election {
-    // Store candidate
-    // Read candidate
-    string public candidate; // declare public gives getter function
-    // Constructor
-     constructor() public {
-        /* public because constructor is going to run 
-        when we deploy our contract to the blockchain */
-        candidate = "Candidate 1";
-        /* state variable: accesible inside of contract,
-        represent data that belongs to entire contract */
-    }
+  // Candidate's model
+  struct Candidate {
+    uint id;
+    string name;
+    uint voteCount;
+  }
 
+  mapping(uint => Candidate) public candidates;
+  //declare public gives getter function
+  //hashing [key:value] pair (id:struct)
+
+  uint public candidatesCount;
+
+  constructor() public {
+    /* public because constructor is going to run 
+    when we deploy our contract to the blockchain */
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
+  }
+
+  function addCandidate(string memory _name) private {
+    candidatesCount++;
+    // mapping(id:Candidate(struct))
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+  }
+  
 }
