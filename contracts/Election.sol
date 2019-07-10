9dff12eaf6247630169b08a551ac35737bbe0141
George Keloglou <geokeloglou@gmail.com>
2019-07-10 04:32:30 +0300

    voting functionality

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,9 +8,11 @@ contract Election {
     uint voteCount;
   }
 
-  mapping(uint => Candidate) public candidates;
+  //hashing [key:value] pair (account:boolean)
+  mapping(address => bool) public voters;
   //declare public gives getter function
   //hashing [key:value] pair (id:struct)
+  mapping(uint => Candidate) public candidates;
 
   uint public candidatesCount;
 
@@ -26,5 +28,20 @@ contract Election {
     // mapping(id:Candidate(struct))
     candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
   }
-  
+
+  function vote(uint _candidateId) public {
+    // require that haven't voted before
+    require(!voters[msg.sender]); // if false stops execution
+
+    // require a valid candidate
+    require(_candidateId > 0 && _candidateId <= candidatesCount);
+    // if execution stopped, gas will be refunded to the sender
+    
+    // we set msg.sender -> account => true
+    // record that voter has voted
+    voters[msg.sender] = true;
+
+    // update candidate vote count
+    candidates[_candidateId].voteCount++;
+  }
 }
